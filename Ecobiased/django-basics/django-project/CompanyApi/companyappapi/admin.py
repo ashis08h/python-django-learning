715cb0244d5379aed8225ff54f333e2e14e3911b
askumar <askumar@starbucks.com>
2024-02-08 16:34:43 +0530

    added admin change code.

--- Ecobiased/django-basics/django-project/CompanyApi/companyappapi/admin.py
+++ Ecobiased/django-basics/django-project/CompanyApi/companyappapi/admin.py
@@ -8,9 +8,11 @@ class CompanyAdmin(admin.ModelAdmin):
     list_display = ['name', 'location', 'type']
     search_fields = ['name']
 
+
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ['name', 'email', 'company']
     list_filter = ['company']
+
 
 admin.site.register(Company, CompanyAdmin)
 admin.site.register(Employee, EmployeeAdmin)
--- Ecobiased/django-basics/django-project/s1project/s1app/admin.py
+++ Ecobiased/django-basics/django-project/s1project/s1app/admin.py
@@ -8,9 +8,20 @@ admin.site.register(Woman)
 admin.site.register(School)
 admin.site.register(SaraswatiVidyaMandir)
 admin.site.register(Employee)
-admin.site.register(Manager)
-admin.site.register(Post)
 admin.site.register(Book)
 admin.site.register(Author)
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    search_fields = ['title']
+
+
+class ManagerAdmin(admin.ModelAdmin):
+    list_display = ['company']
+    list_filter = ['company']
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Manager, ManagerAdmin)
 

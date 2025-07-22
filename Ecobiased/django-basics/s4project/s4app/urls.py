7855a52f66e060f5dfa6e1f70e14d42e2306309a
ashish <ashis08h@gmail.com>
2025-07-23 00:11:15 +0530

    added serializers in s4app.

--- Ecobiased/django-basics/s4project/s4app/urls.py
+++ Ecobiased/django-basics/s4project/s4app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import BookView, ManagerView
+from .views import BookView, ManagerView, SaraswatiVidyaMandirView, SessionView, AuthorView
 
 
 urlpatterns = [
     path('books/', BookView.as_view(), name="books"),
-    path('managers/', ManagerView.as_view(), name='managers')
+    path('managers/', ManagerView.as_view(), name='managers'),
+    path('svm/', SaraswatiVidyaMandirView.as_view(), name="saraswati_vidya_mandir"),
+    path('session/', SessionView.as_view(), name='session'),
+    path('author/', AuthorView.as_view(), name='author')
 ]
--- Ecobiased/django-basics/s4project/s4app/views.py
+++ Ecobiased/django-basics/s4project/s4app/views.py
@@ -3,7 +3,12 @@ from django.views import View
 from django.http import HttpResponse
 from django.db.models import Q
 from django.shortcuts import render
-from .models import Author, Book, Employee, Manager
+from .models import Author, Book, Employee, Manager, SaraswatiVidyaMandir, Man
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from .serializers import AuthorSerializer
 
 # Create your views here.
 
@@ -41,5 +46,57 @@ class ManagerView(View):
         for manager in managers:
             print(manager.employee.all())
         return render(request, 'manager.html', context={'managers': managers})
+
+
+class SaraswatiVidyaMandirView(View):
+
+    def get(self, request):
+        saraswati_vidya_mandir = SaraswatiVidyaMandir.objects.select_related('school').get(id=1)
+        print("saraswati_vidya_mandir", saraswati_vidya_mandir)
+        mans = Man.objects.prefetch_related('person')
+        for man in mans:
+            print("persons", man.person.all())
+        mans = Man.objects.filter(Q(name__icontains='m') & Q(name__startswith='ma'))
+        return render(request, 'saraswati_vidya_mandir.html', context={'mans': mans})
+
+
+class SessionView(View):
+
+    def get(self, request):
+        request.session['username'] = 'ashish'
+        print(f"I have stored the session with name {request.session.get('username', None)}")
+        return HttpResponse(f"I have stored the session with name {request.session.get('username', None)}")
+
+
+@receiver(post_save, sender=Author)
+def send_email(sender, instance, created, **kwargs):
+
+    print("I am from email send function")
+    print("sender", sender)
+    print("instance", instance)
+    print("created", created)
+    for key, value in kwargs.items():
+        print("key", key)
+        print("value", value)
+
+
+class AuthorView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def post(self, request):
+        author_obj = Author()
+        author_obj.name = "custom_author"
+        author_obj.save()
+        return HttpResponse(f"Author with id {author_obj.id} created successfully.")
+
+    def get(self, request):
+
+        authors = Author.objects.all()
+        author_serializers = AuthorSerializer(authors, many=True)
+        print("author_serializers", author_serializers.data)
+        return render(request, 'author.html', context={'authors': author_serializers})
 
 
